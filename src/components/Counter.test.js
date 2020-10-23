2ac33db0b1c944d0c480618594868d02b297d6ff
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-10-23 15:26:51 +0200

    write red tests for click counter challenges

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Counter from "./Counter";
 import { setup, findByTestAttr } from "../testing/test-utils";
 
-
 test("renders without error", () => {
   const wrapper = setup(<Counter />);
   const appComponent = findByTestAttr(wrapper, "component-app");
@@ -23,10 +22,34 @@ test("counter starts at 0", () => {
   const count = findByTestAttr(wrapper, "counter-display").text();
   expect(count).toBe("0");
 });
-test("clicking button increments counter display", () => {
+test("clicking decrement button with counter at 0 does nothing", () => {
+  const wrapper = setup(<Counter />);
+  const decrementButton = findByTestAttr(wrapper, "decrement-button");
+  decrementButton.simulate("click");
+  const count = findByTestAttr(wrapper, "counter-display").text();
+  expect(count).toBe("0");
+});
+test("show error message when clicking decrement button with counter at 0", () => {
+  const wrapper = setup(<Counter />);
+  const errorMessage = findByTestAttr(wrapper, "error-message");
+  expect(errorMessage.length).toBe(1);
+});
+test("clicking increment button increments counter display", () => {
   const wrapper = setup(<Counter />);
   const incrementButton = findByTestAttr(wrapper, "increment-button");
   incrementButton.simulate("click");
   const count = findByTestAttr(wrapper, "counter-display").text();
   expect(count).toBe("1");
+});
+test("clicking increment button clears the error message", () => {
+  const wrapper = setup(<Counter />)
+  const errorMessage = findByTestAttr(wrapper, "error-message")
+  expect(errorMessage.length).toBe(0)
+})
+test("clicking decrement button with counter above 0 decrements count", () => {
+  const wrapper = setup(<Counter />);
+  const decrementButton = findByTestAttr(wrapper, "decrement-button");
+  decrementButton.simulate("click");
+  const count = findByTestAttr(wrapper, "counter-display").text();
+  expect(count).toBe("0");
 });
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Navigation = (props) => {
   return (
-    <nav>
-      <Link to="counter">Counter</Link>
-    </nav>
+    <$Navigation>
+      <$Link to="counter">Counter</$Link>
+    </$Navigation>
   );
 };
+
+const $Navigation = styled.nav`
+  padding: 1rem 0;
+  border-bottom: 1px solid lightgray;
+`;
+
+const $Link = styled(Link)`
+  text-decoration: none;
+  color: black;
+  transition: transform .3s;
+  display: inline-block;
+  &:hover {
+    transform: translateY(3px);
+  }
+`;
 
 export default Navigation;
