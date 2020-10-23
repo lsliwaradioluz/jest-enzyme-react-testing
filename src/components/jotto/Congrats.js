2706ee451c5357b0d2f3a8df79ba12cc4db3d05a
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-10-23 23:46:19 +0200

    create congrats component and make its tests green

--- src/components/jotto/Congrats.js
+++ src/components/jotto/Congrats.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Congrats = (props) => {
-  return <div></div>;
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          {props.success ? "Congratulations! You've guessed the word!" : null}
+        </span>
+      </div>
+    );
+  } else {
+    return <p data-test="component-congrats" />;
+  }
 };
 
 export default Congrats;
