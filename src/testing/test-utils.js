411e00fab7bad3438b42af18194992e0a2dbdf07
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-10-26 13:32:20 +0100

    install Redux Thunk

--- src/testing/test-utils.js
+++ src/testing/test-utils.js
@@ -1,7 +1,8 @@
 import { shallow } from "enzyme";
 import checkPropTypes from "check-prop-types";
 import rootReducer from "../reducers/index";
-import { createStore } from "redux";
+import { middlewares } from "../configureStore";
+import { createStore, applyMiddleware } from "redux";
 
 export const setup = (component) => shallow(component);
 
@@ -19,5 +20,8 @@ export const checkProps = (component, conformingProps) => {
 };
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
