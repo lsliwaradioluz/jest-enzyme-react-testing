06b33d27a18565dcd555c44fefead4b37a80688e
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-10-29 00:21:24 +0100

    Write tests for the Jotto component and connect it to the store

--- src/components/Jotto.js
+++ src/components/Jotto.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
 import Congrats from "./jotto/Congrats";
 import GuessedWords from "./jotto/GuessedWords";
+import { connect } from "react-redux";
+import { getSecretWord } from "../actions/index";
+import Input from "../components/jotto/Input"
 
 class Jotto extends Component {
   render() {
     return (
       <div>
         <h1>Jotto</h1>
-        <Congrats success={false} />
+        <Congrats success={this.props.success} />
+        <Input />
         <GuessedWords
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+          guessedWords={this.props.guessedWords}
         />
       </div>
     );
   }
 }
 
-export default Jotto;
+const mapStateToProps = ({ secretWord, success, guessedWords }) => {
+  return { secretWord, success, guessedWords };
+};
+
+export default connect(mapStateToProps, { getSecretWord })(Jotto);
---
+++ src/components/Jotto.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { storeFactory } from "../testing/test-utils";
+import Jotto from "./Jotto";
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Jotto store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe("redux props", () => {
+  test("receives `secretWord`", () => {
+    const secretWord = "party";
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test("receives `success`", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("receives `guessedWords`", () => {
+    const guessedWords = [{ guessedWord: "train", letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toEqual(guessedWords);
+  });
+  test("receives `getSecretWord`", () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
+});
